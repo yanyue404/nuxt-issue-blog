ee280d10cfc0c3f1d512d18c5e38b4847dbe7488
RainBow <xiaoyueyue165@gmail.com>
2023-04-03 01:39:30 +0800

    feat: 评论展示

--- components/comment.vue
+++ components/comment.vue
@@ -1,35 +1,29 @@
 <template>
   <div>
+    <h2>留言（{{ comments.length }} 条）</h2>
     <ul
       v-for="comment in comments"
       bordered
-      class="rounded-borders q-mt-lg"
+      class="comment rounded-borders q-mt-lg"
       :key="comment.index"
     >
       <div class="q-pa-md">
-        <div avatar>
-          <q-avatar rounded>
+        <div class="item-section">
+          <div class="avatar">
             <img alt="avatar" :src="comment.user.avatar_url" />
-          </q-avatar>
+          </div>
+          <span class="text-weight-bold">{{ comment.user.login }}</span>
+          <span class="text-gray-light">
+            {{ comment.updated_at | timeAgo }}</span
+          >
         </div>
-        <div>
-          <q-item-label lines="1">
-            <span class="text-weight-bold">{{ comment.user.login }}</span>
-            <span class="text-gray-light">
-              {{ comment.updated_at | timeAgo }}</span
-            >
-          </q-item-label>
-          <q-item-label
-            v-html="comment.body_html"
-            class="q-pt-sm"
-          ></q-item-label>
-        </div>
+        <div v-html="comment.body_html" class="q-pt-sm"></div>
       </div>
     </ul>
     <div class="comment-btn">
       <button outline no-caps class="rainbow" @click="goAddComment">
         <div left size="2rem" name="add_comment" />
-        <div>Add Comment</div>
+        <div class="text">Add Comment</div>
       </button>
     </div>
   </div>
@@ -48,7 +42,6 @@ export default {
   },
   filters: {
     timeAgo(d) {
-      debugger;
       return formatPassTime(new Date(d));
     },
   },
@@ -73,20 +66,63 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.q-item__section--side > .q-avatar {
-  font-size: 48px;
+ul {
+  padding-left: 0;
 }
-@media (max-width: 767px) {
-  .q-item__section--side > .q-avatar {
-    font-size: 36px;
-  }
+.q-pa-md {
+  padding: 16px 16px;
+}
+.q-pt-sm {
+  padding-top: 8px;
+}
+.text-weight-bold {
+  font-weight: 700;
 }
 .comment-btn {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-  margin-top: 48px;
+  margin-top: 30px;
   padding-bottom: 30px;
+}
+.comment {
+  border-bottom: 1px dashed #d3d3d3;
+}
+.item-section {
+  display: flex;
+  align-items: center;
+  span {
+    margin-left: 10px;
+  }
+}
+.rounded-borders {
+  border-radius: 4px;
+}
+.avatar {
+  position: relative;
+  vertical-align: middle;
+  display: inline-block;
+  border-radius: 50%;
+  font-size: 48px;
+  height: 1em;
+  width: 1em;
+  img {
+    border: none !important;
+  }
+}
+button {
+  background: #fff;
+  outline: 0;
+  cursor: pointer;
+  border-radius: 3px;
+  border: 0;
+  box-shadow: 0 1px 5px #0003, 0 2px 2px #00000024, 0 3px 1px -2px #0000001f;
+}
+.text {
+  padding: 4px 16px;
+  font-size: 1em;
+  line-height: 24px;
+  font-weight: 500;
 }
 </style>
